fb9f61b10129115a59115e4e3dfb7ab69f0d0332
Dipanshu MITTAL <dipanshumittal466@gmail.com>
2025-09-11 11:52:47 +0530

    Update Home.jsx

--- HomeServicesEtc_fullstack_render_ready/frontend/src/pages/Home.jsx
+++ HomeServicesEtc_fullstack_render_ready/frontend/src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Layout from "../components/Layout";
+import categories from "../data/categories.json";
+
 
 export default function Home() {
   return (
