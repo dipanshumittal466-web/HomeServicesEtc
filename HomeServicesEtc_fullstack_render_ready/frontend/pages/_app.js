1089f7380ca34878c9272878d77fb9d7ed1c7301
Dipanshu MITTAL <dipanshumittal466@gmail.com>
2025-09-13 17:39:19 +0530

    Update _app.js

--- HomeServicesEtc_fullstack_render_ready/frontend/pages/_app.js
+++ HomeServicesEtc_fullstack_render_ready/frontend/pages/_app.js
@@ -2,11 +2,22 @@ import '../styles/globals.css'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
-import '../styles/globals.css'
-export default function App({Component, pageProps}) {
+export default function App({ Component, pageProps }) {
+  // Load categories mapping on client side
+  if (typeof window !== 'undefined') {
+    fetch('/assets/categories_mapping.json')
+      .then(r => r.json())
+      .then(m => {
+        window.__CATEGORIES_MAPPING__ = m
+      })
+      .catch(() => {})
+  }
 
-if (typeof window !== 'undefined') { fetch('/assets/categories_mapping.json').then(r=>r.json()).then(m=>window.__CATEGORIES_MAPPING__ = m).catch(()=>{}); }
- return (<div><Header/><Component {...pageProps} /><Footer/></div>); }
-
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+  )
 }
