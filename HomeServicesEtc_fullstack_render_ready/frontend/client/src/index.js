d23931e245fa32d2058f05d41034326b83737061
Dipanshu MITTAL <dipanshumittal466@gmail.com>
2025-09-13 14:29:50 +0530

    Update index.js

--- HomeServicesEtc_fullstack_render_ready/frontend/client/src/index.js
+++ HomeServicesEtc_fullstack_render_ready/frontend/client/src/index.js
@@ -1,7 +1,7 @@
 // client/src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+
 
 import App from './App';
 import './index.css';
