6df45a2d5f69984225126d16ad08deea699843b7
Dipanshu MITTAL <dipanshumittal466@gmail.com>
2025-09-13 14:41:35 +0530

    Update index.js

--- HomeServicesEtc_fullstack_render_ready/frontend/client/src/index.js
+++ HomeServicesEtc_fullstack_render_ready/frontend/client/src/index.js
@@ -1,19 +1,17 @@
 // client/src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { BrowserRouter } from 'react-router-dom';   // ✅ Added import
 
 import App from './App';
 import './index.css';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root')
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    
+    <BrowserRouter>
       <App />
-   
+    </BrowserRouter>
   </React.StrictMode>
 );
