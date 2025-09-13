fe0b1a1093308c85ece8008d02078e66b4b08aec
Dipanshu MITTAL <dipanshumittal466@gmail.com>
2025-09-13 11:31:38 +0530

    Update api.js

--- HomeServicesEtc_fullstack_render_ready/frontend/client/src/services/api.js
+++ HomeServicesEtc_fullstack_render_ready/frontend/client/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || 'https://homeservicesetc.onrender.com/api'
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/api'
+});
+
 });
 
 export default api;
